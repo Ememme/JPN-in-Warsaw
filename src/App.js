5a6ef61766aa560ebc30dc887d2321d14bfb67ac
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-08-07 18:47:03 +0200

    Feat: add footer component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import GoogleMap from 'google-map-react';
 import Map from './Components/map';
 import mapStyle from './mapStyle.json'
+import Footer from './Components/Footer'
 import logo from './logo.svg';
 import './App.css';
 
@@ -22,6 +23,7 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Map style={mapStyle}/>
+        <Footer />
       </div>
     );
   }
