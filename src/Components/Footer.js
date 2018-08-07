e3d94f6700005d627574f3e482a61e13e999cfd8
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-08-07 18:45:50 +0200

    Basic footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="credits">
+            <p>Designed by
+                <a href="https://www.linkedin.com/in/magdamieczynska/"> MagdaM</a>
+                 <span className="highlight">|</span> Data by
+                <a href="www.zomato.com"> Zomato </a>
+            </p>
+            <p>All icons from <a href="https://www.flaticon.com/" title="Flaticon">Flaticon</a>
+            </p>
+        </div>
+    )
+}
+
+export default Footer
