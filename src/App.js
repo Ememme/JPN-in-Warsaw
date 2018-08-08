6a22d30ba2680033ea429db4757b6ad288fb88fb
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-08-08 14:34:31 +0200

    Get data from ZOMATO API (first 20 results)

--- src/App.js
+++ src/App.js
@@ -8,9 +8,33 @@ import './App.css';
 
 class App extends Component {
 
-  componentDidMount() {
-    console.log(mapStyle)
+  state = {
+    allRestaurants: [],
+    error: false
   }
+  componentDidMount() {
+    this.getZomatoData()
+  }
+
+
+  getZomatoData() {
+    fetch('https://developers.zomato.com/api/v2.1/search?entity_id=96681&entity_type=subzone&start=1&count=30&lat=52.22967&lon=21.0122287&radius=5000&cuisines=60&sort=rating&order=asc',{
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'user-key': '2993ad9395ee17dae3ca81552220ac5b'
+    }})
+    .then((response) => {
+      return response.json();
+    })
+    .then(fromJSON => {
+    const allRestaurants = fromJSON.restaurants
+      this.setState({
+        allRestaurants: allRestaurants
+      })
+    })
+  };
+
 
   render() {
     return (
@@ -22,7 +46,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Map style={mapStyle}/>
+        {/* <Map style={mapStyle}/> */}
         <Footer />
       </div>
     );
@@ -30,3 +54,6 @@ class App extends Component {
 }
 
 export default App;
+//
+// zomatorequest:
+// curl -X GET --header "Accept: application/json" --header "user-key: 2993ad9395ee17dae3ca81552220ac5b" "https://developers.zomato.com/api/v2.1/search?entity_id=96681&entity_type=subzone&start=1&count=30&lat=52.22967&lon=21.0122287&radius=5000&cuisines=60&sort=rating&order=asc"
