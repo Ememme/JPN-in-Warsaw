0752b363ed4eccfe4b968df00dfcbfcf2c7672f2
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-08-09 17:08:37 +0200

    Update: add logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import '../App.css'
+import Logo from '../Assets/kanji-symbol-of-japan.svg'
 
 const Header = () => {
   return (
     <header className="app-header">
+      <div className="app-logo"><img src={Logo} alt="Kanji symbol of Japan" style={{width: '80px', height: '80px' }}/></div>
       <h1 className="app-title">Japanese Restaurants in Warsaw</h1>
     </header>
   )
