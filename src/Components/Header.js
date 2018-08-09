7ac5edbe28b557e3ba7e5ca88c24a56e0a04bfce
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-08-09 13:02:55 +0200

    Feat: Header compoenent with basic styling applied

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../App.css'
+
+const Header = () => {
+  return (
+    <header className="app-header">
+      <h1 className="app-title">Japanese Restaurants in Warsaw</h1>
+    </header>
+  )
+}
+
+export default Header
