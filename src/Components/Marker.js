2b51e857865a1aefd5ba47ac98cf3476d77982ef
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-08-14 17:50:00 +0200

    Add console.log for marker

--- src/Components/Marker.js
+++ src/Components/Marker.js
@@ -2,10 +2,12 @@ import React, {Component} from 'react';
 import Icon from '../Assets/marker_black.svg'
 
 const Marker = props => {
-	
-  return (<div className="marker">
-    <img src={Icon} alt="marker"/>
-  </div>)
+
+  return (
+		<div className="marker" onClick={() => console.log(props.name, props.district)}>
+    	<img src={Icon} alt="marker"/>
+  	</div>
+	)
 }
 
 export default Marker;
