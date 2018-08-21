7d355a46c219d4555e44d0c84208934976acc7b3
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-08-21 14:31:12 +0200

    Add toggle feature to hamburger menu

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,49 @@
-import React from 'react'
+import React, {Component} from 'react'
 import '../App.css'
 import Logo from '../Assets/kanji-symbol-of-japan.svg'
 
-const Header = () => {
-  return (
-    <header className="app-header">
-      <div className="button-wrapper">
-        <button id="menu" className="hamburger" type="button">
-          <span className="hamburger-box hamburger--spring">
-            <span className="hamburger-inner"></span>
-          </span>
-        </button>
-      </div>
-      <div className="app-logo"><img src={Logo} alt="Kanji symbol of Japan" style={{width: '80px', height: '80px' }}/></div>
-      <h1 className="app-title">Japanese Restaurants in Warsaw</h1>
-    </header>
-  )
+class Header extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      // 'is-active' class is used to toggle hamburger menu with CSS
+      addActiveClass: false,
+      // ariaExpanded to indicate to visually impaired users whether the menu is opened or closed.
+      ariaExpanded: false,
+      // flag for sidebar
+      menuOpen: false
+    }
+  }
+  // Function based on this example: https://codepen.io/JorgeGWD/pen/jYMVXY
+  toggleActive() {
+    console.log('active!')
+    this.setState({
+      addActiveClass: !this.state.addActiveClass,
+      ariaExpanded: !this.state.ariaExpanded,
+      menuOpen: !this.state.menuOpen
+    });
+
+  }
+  render() {
+    let menuClasses = ["hamburger", "hamburger--spring"];
+    if (this.state.addActiveClass) {
+      menuClasses.push("is-active");
+    }
+    return (
+      <header className="app-header">
+        <div className="button-wrapper">
+          <button id="menu" className={menuClasses.join(' ')} type="button" aria-label="Menu" aria-controls="navigation" aria-expanded="true/false" onClick={this.toggleActive.bind(this)}>
+            <span className="hamburger-box">
+              <span className="hamburger-inner"></span>
+            </span>
+          </button>
+        </div>
+        <div className="app-logo"><img src={Logo} alt="Kanji symbol of Japan" style={{width: '80px', height: '80px' }}/></div>
+        <h1 className="app-title">Japanese Restaurants in Warsaw</h1>
+      </header>
+    )
+  }
 }
 
 export default Header
