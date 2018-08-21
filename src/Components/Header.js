851515cce847909b6a2444b727569aa0be4aff38
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-08-21 12:30:51 +0200

    Add hamburger icon to header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,6 +5,13 @@ import Logo from '../Assets/kanji-symbol-of-japan.svg'
 const Header = () => {
   return (
     <header className="app-header">
+      <div className="button-wrapper">
+        <button id="menu" className="hamburger" type="button">
+          <span className="hamburger-box hamburger--spring">
+            <span className="hamburger-inner"></span>
+          </span>
+        </button>
+      </div>
       <div className="app-logo"><img src={Logo} alt="Kanji symbol of Japan" style={{width: '80px', height: '80px' }}/></div>
       <h1 className="app-title">Japanese Restaurants in Warsaw</h1>
     </header>
