e2edac3d0b6d9bd70b177cf772b732fe19377518
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-08-21 17:32:13 +0200

    Feat: add SideBar component

---
+++ src/Components/SideBar.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const SideBar = (props) => {
+  console.log(props)
+  return (
+
+    <div className='sidebar'>
+      <div className='sidebar-heading'><h2>List of restaurants</h2></div>
+      <div className="district-selection">
+        <select value={props.district} aria-label="Find by district" >
+        <option value="blank" disabled>Find a restaurant in your area</option>
+        <option value="All">All</option>
+        <option value="Śródmieście Północne">Śródmieście Północne</option>
+        <option value="Śródmieście Północne">Śródmieście Południowe</option>
+        <option value="Powiśle">Powiśle</option>
+        <option value="Wola">Wola</option>
+        </select>
+    </div>
+      {/* <ul className="restaurant-in-district" id="district-list">
+
+      </ul> */}
+    </div>
+  )
+  }
+export default SideBar;
+// onChange={event => props.filterPlaces(event.target.value)}
