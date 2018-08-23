ccebc6ec57969ccea0601e0b46636bf93c1be2d2
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-08-23 10:28:22 +0200

    Refactor to classto have state in SideBar

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -1,26 +1,29 @@
-import React from 'react'
+import React, { Component} from 'react'
 
-const SideBar = (props) => {
-  console.log(props)
-  return (
+class SideBar extends Component {
+  
+  render() {
+    return (
 
-    <div className='sidebar'>
-      <div className='sidebar-heading'><h2>List of restaurants</h2></div>
-      <div className="district-selection">
-        <select value={props.district} aria-label="Find by district" >
-        <option value="blank" disabled>Find a restaurant in your area</option>
-        <option value="All">All</option>
-        <option value="Śródmieście Północne">Śródmieście Północne</option>
-        <option value="Śródmieście Północne">Śródmieście Południowe</option>
-        <option value="Powiśle">Powiśle</option>
-        <option value="Wola">Wola</option>
-        </select>
-    </div>
-      {/* <ul className="restaurant-in-district" id="district-list">
+      <div className='sidebar'>
+        <div className='sidebar-heading'><h2>List of restaurants</h2></div>
+        <div className="district-selection">
+          <select value={this.props.district} aria-label="Find by district" >
+          <option value="blank" disabled>Find a restaurant in your area</option>
+          <option value="All">All</option>
+          <option value="Śródmieście Północne">Śródmieście Północne</option>
+          <option value="Śródmieście Północne">Śródmieście Południowe</option>
+          <option value="Powiśle">Powiśle</option>
+          <option value="Wola">Wola</option>
+          </select>
+      </div>
+        {/* <ul className="restaurant-in-district" id="district-list">
 
-      </ul> */}
-    </div>
-  )
+        </ul> */}
+      </div>
+    )
   }
+
+}
 export default SideBar;
 // onChange={event => props.filterPlaces(event.target.value)}
