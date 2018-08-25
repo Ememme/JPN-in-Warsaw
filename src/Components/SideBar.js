5330c49116495084ed9057c5b6bd6bde8586fdf2
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-08-25 22:08:14 +0200

    Add aria to sidebar component

--- src/Components/SideBar.js
+++ src/Components/SideBar.js
@@ -18,7 +18,7 @@ class SideBar extends Component {
         onChange={this.handleOnChange.bind(this)}
         >
         <div className='sidebar-heading'><h2>List of restaurants</h2></div>
-        <div className="district-selection">
+        <div className="district-selection" aria-role="listbox">
           <select value={this.props.districtRestaurants} aria-label="Find by district" >
           <option value="blank" disabled>Find a restaurant in your area</option>
           <option value="All">All</option>
@@ -33,11 +33,14 @@ class SideBar extends Component {
               {this.props.locations.map(item =>
               <li key={item.restaurant.R.res_id}
                 onClick={() => openInfoWindow(item.restaurant.R.res_id)}
-                className='location'>
-                <h4 className='location-name'>
+                className='location'
+                aria-label={item.restaurant.name}
+                role="button"
+                tabIndex="0"
+                >
+                <h3 className='location-name'>
                   {item.restaurant.name}
-                </h4>
-
+                </h3>
               </li>)}
         </ul>
       </div>
