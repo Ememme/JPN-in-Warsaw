c45bd503e3b5f9f3c742ee6f10e9ce2a4cf4cccb
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-08-25 22:10:13 +0200

    Remove commented code

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,37 +1,18 @@
 import React from 'react';
-// from ReactBootCamp by T.McGinnis
+
 class Loading extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     text: 'Loading data from ZOMATO'
-  //   };
-  // }
-  // componentDidMount() {
-  //   const stopper = this.state.text + '...';
-  //   this.interval = window.setInterval(() => {
-  //     this.state.text === stopper
-  //       ? this.setState(() => ({ text: 'Loading data from ZOMATO' }))
-  //       : this.setState((prevState) => ({ text: prevState.text + '.' }))
-  //   }, 300)
-  // }
-  // componentWillUnmount() {
-  //   window.clearInterval(this.interval);
-  // }
+
   render() {
 
     return (
-
-    <div className="loader">
+      <div className="loader">
       <p>Loading data from ZOMATO</p>
       <div className="sk-double-bounce">
         <div className="sk-child sk-double-bounce1"></div>
         <div className="sk-child sk-double-bounce2"></div>
       </div>
     </div>
-
-    )
-
+    )  
   }
 }
 
