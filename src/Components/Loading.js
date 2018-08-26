d407de15f78618f14af7e47b7bccb185fc3da66b
Magda Mieczynska <magda.mieczynska@gmail.com>
2018-08-26 11:46:21 +0200

    Refactor to stateless componenet syntax

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,27 +1,9 @@
 import React from 'react';
-// from ReactBootCamp by T.McGinnis
-class Loading extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     text: 'Loading data from ZOMATO'
-  //   };
-  // }
-  // componentDidMount() {
-  //   const stopper = this.state.text + '...';
-  //   this.interval = window.setInterval(() => {
-  //     this.state.text === stopper
-  //       ? this.setState(() => ({ text: 'Loading data from ZOMATO' }))
-  //       : this.setState((prevState) => ({ text: prevState.text + '.' }))
-  //   }, 300)
-  // }
-  // componentWillUnmount() {
-  //   window.clearInterval(this.interval);
-  // }
-  render() {
+
+const Loading = () =>  {
+
 
     return (
-
     <div className="loader">
       <p>Loading data from ZOMATO</p>
       <div className="sk-double-bounce">
@@ -29,10 +11,7 @@ class Loading extends React.Component {
         <div className="sk-child sk-double-bounce2"></div>
       </div>
     </div>
-
     )
-
-  }
 }
 
 export default Loading;
